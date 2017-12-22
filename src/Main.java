a9deffb13b2d895f4548e8f9acb5e198b1248a52
Gabriela Motta <gabriela.oliveira@ccc.ufcg.edu.br>
2017-12-21 22:38:13 -0300

    ajustes normal

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) {
-		Simulador sim = new Simulador(30, 10, 1, new double[]{0.5, 0.4}, TipoDistribuicao.NORMAL);
+		Simulador sim = new Simulador(30, 10, 1, new double[]{0.5, 0.7}, TipoDistribuicao.NORMAL);
 		
 		try {
 			FileWriter fw = new FileWriter("resultados_normal.txt");
